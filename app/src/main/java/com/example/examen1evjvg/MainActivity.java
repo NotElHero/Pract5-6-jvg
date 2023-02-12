b20c8d210cbf0487ea4122836d05d6d9fc681a43
Javier Villacorta <javier.vilgar@educa.jcyl.es>
2023-02-12 21:17:36 +0100

    v2 barely done

--- app/src/main/java/com/example/examen1evjvg/MainActivity.java
+++ app/src/main/java/com/example/examen1evjvg/MainActivity.java
@@ -14,20 +14,25 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button botonConvertir, botonInfo;
+    Button botonConvertir, botonInfo, botonHistorial;
     EditText cantidad;
     RadioGroup monedasOrigen, monedasDestino;
     RadioButton radioEuroOrigen, radioDolarOrigen, radioBitcoinOrigen, radioEuroDestino, radioDolarDestino, radioBitcoinDestino;
     String seleccionOrigen = "";
     String seleccionDestino = "";
-    TextView textConversion;
+    TextView textConversion, textMoneda;
     int selectedOrigen;
     int selectedDestino;
+    float conversion, cantidadFloat;
     boolean haConvertido = false;
     String url = "https://coinmarketcap.com/es/currendies/bitcoin/";
+    ArrayList<Float> historialConversiones = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +43,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         botonConvertir.setOnClickListener(this);
         botonInfo = findViewById(R.id.botonInfo);
         botonInfo.setOnClickListener(this);
+        botonHistorial = findViewById(R.id.botonHistorial);
+        botonHistorial.setOnClickListener(this);
 
         textConversion = findViewById(R.id.textViewConversion);
+        textMoneda = findViewById(R.id.textView7);
         cantidad = findViewById(R.id.editText);
         monedasOrigen = findViewById(R.id.radioOrigen);
         monedasDestino = findViewById(R.id.radioDestino);
@@ -71,7 +79,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (seleccionDestino == "" || seleccionOrigen == ""){
                     textConversion.setText("Por favor, escoge una moneda");
                 } else{
-                    textConversion.setText(cantidad.getText().toString() + seleccionOrigen + cantidad.getText().toString() + seleccionDestino);
+                    hacerConversion();
+                    textConversion.setText((int) conversion);
+                    historialConversiones.add(conversion);
                     haConvertido = true;
                 }
                 if (haConvertido){
@@ -79,12 +89,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.botonInfo:
-
                     botonInfo.setVisibility(View.VISIBLE);
                     Uri uri = Uri.parse(url);
                     Intent link = new Intent(Intent.ACTION_VIEW, uri);
                     startActivity(link);
-
+                break;
+            case R.id.botonHistorial:
+                Toast historial = Toast.makeText(getApplicationContext(), "Tus ultimas conversiones: ", Toast.LENGTH_LONG); //me falta tiempo
+                historial.show();
                 break;
         }
     }
@@ -92,13 +104,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public String seleccionesDestino(){
         selectedDestino = monedasDestino.getCheckedRadioButtonId();
         if (selectedDestino == R.id.radioEuroDestino){
-            seleccionDestino = " euros";
+            seleccionDestino = "euros";
         }
         if (selectedDestino == R.id.radioDolarDestino){
-            seleccionDestino = " dólares";
+            seleccionDestino = "dolares";
         }
         if (selectedDestino == R.id.radioBitcoinDestino){
-            seleccionDestino = " bitcoins";
+            seleccionDestino = "bitcoins";
         }
         if (selectedDestino == 0){
             seleccionDestino = "";
@@ -109,15 +121,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String hint;
         selectedOrigen = monedasOrigen.getCheckedRadioButtonId();
         if (selectedOrigen == R.id.radioEuroOrigen){
-            seleccionOrigen = " euros son ";
-            hint = "Euros";
+            seleccionOrigen = "euros";
+            textMoneda.setText("€");
+            cantidad.setHint("Euros");
         }
         if (selectedOrigen == R.id.radioDolarOrigen){
-            seleccionOrigen = " dólares son ";
+            seleccionOrigen = "dolares";
+            textMoneda.setText("$");
             cantidad.setHint("Dólares");
         }
         if (selectedOrigen == R.id.radioBitcoinOrigen){
-            seleccionOrigen = " bitcoins son ";
+            seleccionOrigen = "bitcoins";
+            textMoneda.setText("\u20BF");
             cantidad.setHint("Bitcoins");
         }
         if (selectedOrigen == 0){
@@ -125,5 +140,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             cantidad.setHint("Importe");
         }
         return seleccionOrigen;
+    }
+
+    public float hacerConversion(){
+        //febrero de 2023
+        Toast adv = Toast.makeText(getApplicationContext(), "No puedes convertir la misma moneda", Toast.LENGTH_SHORT);
+        cantidadFloat = Float.parseFloat(cantidad.getText().toString());
+        if (seleccionDestino == "euros"){
+            if (seleccionOrigen == "euros"){
+                adv.show();
+            }
+            if (seleccionOrigen == "dolares"){
+                conversion = (float) (cantidadFloat * 1.07);
+            }
+            if (seleccionOrigen == "bitcoins"){
+                conversion = (float) (cantidadFloat * 0.000048);
+            }
+        }
+
+        if (seleccionDestino == "dolares"){
+            if (seleccionOrigen == "dolares"){
+                adv.show();
+            }
+            if (seleccionOrigen == "euros"){
+                conversion = (float) (cantidadFloat / 1.07);
+            }
+            if (seleccionOrigen == "bitcoins"){
+                conversion = (float) (cantidadFloat * 0.000048);
+            }
+        }
+
+        if (seleccionDestino == "bitcoins"){
+            if (seleccionOrigen == "bitcoins"){
+                adv.show();
+            }
+            if (seleccionOrigen == "euros"){
+                conversion = (float) (cantidadFloat / 1.07);
+            }
+            if (seleccionOrigen == "dolares"){
+                conversion = (float) (cantidadFloat * 0.000045);
+            }
+        }
+        return conversion;
     }
 }
